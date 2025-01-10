514dbc5da436b0a0c04843fb868d90a2755905ff
vasanthcodes <vasanthsanku2719@gmail.com>
2025-01-10 18:07:43 +0530

    add new files

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+//create a function which will get the computer choice
+function GetComputerChoice(max = 3) {
+  return Math.floor(Math.random() * max);
+}
+console.log(GetComputerChoice())
+//take an input from the user
+function inputfromuser(){
+    prompt("enter your choice")
+}
+let computerscore=0;
+let humanscore=0;
+
+function Playround(humanChoice,ComputerChoice){
+    
+}
+
+
+//check the input and the user value
+// display the winner
